fe4bbc55dfbad6b342372859ce927434f0e9b0b5
ronald1710 <ronald.azofeifa@yahoo.com>
2022-08-24 03:31:47 +0000

    generador escusas

--- src/app.js
+++ src/app.js
@@ -5,7 +5,26 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let who = ["the dog", "my granma", "his turtle", "my bird"];
+let what = ["eat", "pissed", "crushed", "broked"];
+let when = [
+  "before the class",
+  "right in time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
+function generadorExcusa(quien, que, cuando) {
+  let numeroQuien = Math.floor(Math.random() * quien.length);
+  let numeroQue = Math.floor(Math.random() * que.length);
+  let numeroCuando = Math.floor(Math.random() * cuando.length);
+  return (
+    quien[numeroQuien] + " " + que[numeroQue] + " " + " " + cuando[numeroCuando]
+  );
+}
+function onLoad() {
+  var excusa = document.getElementById("excuse");
+  excusa.innerHTML = generadorExcusa(who, what, when);
+}
+onload = onLoad;
